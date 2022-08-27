0a216d3a5e089f8a91c89feda686f604824b3871
sachinmagadum <sachinmagadum5009@gmail.com>
2022-08-27 09:41:43 +0530

    Added code for add & sub

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+def add(a,b):
+    return a+b
+
+print(f"Addition of {a} and {b} id {add(a,b)}")
+
+
+def sub(a,b):
+    return a-b
+
+print(f"Substraction of {a} and {b} id {sub(a,b)}")
